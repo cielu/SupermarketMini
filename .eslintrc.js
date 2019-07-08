2562812b1c3eb23d5e75ca1d05f5e57f718a7d84
ciel yu <ciel7yu@gmail.com>
2019-07-09 00:43:01 +0800

    新增随banner色动态改变导航栏背景色，首页tabs改为横向滚动条，新增滚动进度条

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,6 +21,7 @@ module.exports = {
   'rules': {
     // allow paren-less arrow functions
     'arrow-parens': 0,
+    'indent': ['error',4],
     // allow async-await
     'generator-star-spacing': 0,
     // allow debugger during development
