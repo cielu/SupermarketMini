8b33d99b1a1fb35094a21bccd1d7754900456f47
ciel yu <ciel7yu@gmail.com>
2019-07-18 10:43:41 +0800

    新增确认订单，订单详情页，其他优化

--- src/mixins/globalData.js
+++ src/mixins/globalData.js
@@ -2,9 +2,11 @@
 export default {
     data: {
         StatusBarHeight: 50,
+        isIPhoneX: false,
         userInfo: null
     },
     created () {
+        this.isIPhoneX = this.$app.$options.globalData.isIPhoneX
         this.StatusBarHeight = this.$app.$options.globalData.StatusBarHeight
         this.userInfo = this.$app.$options.globalData.userInfo
     }
--- wepy.config.js
+++ wepy.config.js
@@ -1,37 +1,36 @@
-const path = require('path');
-var prod = process.env.NODE_ENV === 'production';
+const path = require('path')
+var prod = process.env.NODE_ENV === 'production'
 
 module.exports = {
-  wpyExt: '.wpy',
-  eslint: true,
-  cliLogs: !prod,
-  build: {
-  },
-  resolve: {
-    alias: {
+    wpyExt: '.wpy',
+    eslint: true,
+    cliLogs: !prod,
+    build: {
+    },
+    resolve: {
+        alias: {
       // counter: path.join(__dirname, 'src/components/counter'),
-      '@': path.join(__dirname, 'src')
+            '@': path.join(__dirname, 'src')
+        },
+        aliasFields: ['wepy', 'weapp'],
+        modules: ['node_modules']
     },
-    aliasFields: ['wepy', 'weapp'],
-    modules: ['node_modules']
-  },
-  compilers: {
-    less: {
-      compress: prod
+    compilers: {
+        less: {
+            compress: prod
+        },
+        babel: {
+            sourceMap: true,
+            presets: [
+                '@babel/preset-env'
+            ],
+            plugins: [
+                '@wepy/babel-plugin-import-regenerator'
+            ]
+        }
     },
-    babel: {
-      sourceMap: true,
-      presets: [
-        '@babel/preset-env'
-      ],
-      plugins: [
-        '@wepy/babel-plugin-import-regenerator'
-      ]
+    plugins: [],
+    appConfig: {
+        noPromiseAPI: ['createSelectorQuery']
     }
-  },
-  plugins: [],
-  appConfig: {
-    noPromiseAPI: ['createSelectorQuery']
-  }
 }
-
