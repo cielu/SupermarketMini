f6c56651ad80c1feffb454579d964841b33fb6b9
ciel yu <ciel7yu@gmail.com>
2019-09-01 19:19:26 +0800

    update gitignore

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,7 +21,7 @@ module.exports = {
   'rules': {
     // allow paren-less arrow functions
     'arrow-parens': 0,
-    'indent': ['error',4],
+    'indent': ['error',2],
     // allow async-await
     'generator-star-spacing': 0,
     // allow debugger during development
